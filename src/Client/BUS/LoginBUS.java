3c17cc7722887834948f28d0bc4827288f8f98be
thanghdheo <hoducthangtn2nhvt@gmail.com>
2021-12-12 21:11:45 +0700

    boyeu2

--- src/Client/BUS/LoginBUS.java
+++ src/Client/BUS/LoginBUS.java
@@ -12,7 +12,7 @@ public class LoginBUS {
     public int login(String usr, String pwd) {
         try {
             pwd = BUS.getMd5(pwd); // mã hoá mật khẩu
-            // kết nối server
+            // kết nối serverrrrrrr
             BUS.connect();
             // gửi tín hiệu đăng nhập và usr, pwd
             BUS.writeLine(Key.DANGNHAP);
